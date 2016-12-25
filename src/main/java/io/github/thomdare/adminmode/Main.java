9adb5fa8f64fe57b05b4b3eb20af85d6fc0aa1cd
cccm5 <danieldavidson@aol.com>
2016-12-24 22:24:20 -0500

    Several features
    Save inventory when entering modes
    Prevent entering modes while in other modes

--- src/main/java/io/github/thomdare/adminmode/Main.java
+++ src/main/java/io/github/thomdare/adminmode/Main.java
@@ -9,103 +9,146 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	private List<String> adminmodeenable = new ArrayList<String>();
-	private List<String> modmodeenable = new ArrayList<String>();
-	private List<String> adminmodedisable = new ArrayList<String>();
-	private List<String> modmodedisable = new ArrayList<String>();
-	private List<String> inMod = new ArrayList<String>();
-	private List<String> inAdmin = new ArrayList<String>();
-	private HashMap<String, Location> StaffLocations = new HashMap<String, Location>();
+    private List<String> adminmodeenable = new ArrayList<String>();
+    private List<String> modmodeenable = new ArrayList<String>();
+    private List<String> adminmodedisable = new ArrayList<String>();
+    private List<String> modmodedisable = new ArrayList<String>();
+    private List<String> inMod = new ArrayList<String>();
+    private List<String> inAdmin = new ArrayList<String>();
+    private HashMap<String, Location> StaffLocations = new HashMap<String, Location>();
+    private HashMap<String, PlayerInventory> StaffInventories = new HashMap<String, PlayerInventory>();
+    private HashMap<String, Integer> StaffExp = new HashMap<String, Integer>();
 
+    public void onEnable() {
+        // We need these
+        getConfig().addDefault("permissions.adminmode.onenable", adminmodeenable);
+        getConfig().addDefault("permissions.modmode.onenable", modmodeenable);
+        getConfig().addDefault("permissions.adminmode.ondisable", adminmodedisable);
+        getConfig().addDefault("permissions.modmode.ondisable", modmodedisable);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        // Lets get those already there
+        adminmodeenable = getConfig().getStringList("permissions.adminmode.onenable");
+        modmodeenable = getConfig().getStringList("permissions.modmode.onenable");
+        adminmodedisable = getConfig().getStringList("permissions.adminmode.ondisable");
+        modmodedisable = getConfig().getStringList("permissions.modmode.ondisable");
+    }
 
-	public void onEnable() {
-		// We need these
-		getConfig().addDefault("permissions.adminmode.onenable", adminmodeenable);
-		getConfig().addDefault("permissions.modmode.onenable", modmodeenable);
-		getConfig().addDefault("permissions.adminmode.ondisable", adminmodedisable);
-		getConfig().addDefault("permissions.modmode.ondisable", modmodedisable);
-		getConfig().options().copyDefaults(true);
-		saveConfig();
-		// Lets get those already there
-		adminmodeenable = getConfig().getStringList("permissions.adminmode.onenable");
-		modmodeenable = getConfig().getStringList("permissions.modmode.onenable");
-		adminmodedisable = getConfig().getStringList("permissions.adminmode.ondisable");
-		modmodedisable = getConfig().getStringList("permissions.modmode.ondisable");
-	}
+    public void onDisable() {
+        adminmodeenable.clear();
+        adminmodedisable.clear();
+        modmodeenable.clear();
+        modmodedisable.clear();
+        inMod.clear();
+        inAdmin.clear();
+        StaffLocations.clear();
+    }
 
-	public void onDisable() {
-		adminmodeenable.clear();
-		adminmodedisable.clear();
-		modmodeenable.clear();
-		modmodedisable.clear();
-		inMod.clear();
-		inAdmin.clear();
-		StaffLocations.clear();
-	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You must be a player to execute that command!");
+            return true;
+        }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-			if (cmd.getName().equalsIgnoreCase("modmode")) {
-				if (player.hasPermission("adminmode.mod")) {
-					if (inMod.contains(player.getName())) {
-						for (String command : modmodedisable) {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
-						}
-						inMod.remove(player.getName());
-						if (StaffLocations.containsKey(player.getName())) {
-							player.teleport(StaffLocations.get(player.getName()));
-							StaffLocations.remove(player.getName());
-						}
-						
-						return true;
-					} else {
-						for (String command : modmodeenable) {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+        Player player = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("modmode")) {
+            if (!player.hasPermission("adminmode.mod")) {
+                sender.sendMessage("You don't have permission to do that");
+                return true;
+            }
+            if (inAdmin.contains(player.getName())) {
+                sender.sendMessage("You're already in admin mode!");
+                return true;
+            }
 
-						}
-						inMod.add(player.getName());
-						StaffLocations.put(player.getName(), player.getLocation());
-						getLogger().info(player.getName() + " entered mod mode");
-						return true;
-					}
-				}
-			} else if (cmd.getName().equalsIgnoreCase("adminmode")) {
-				if (player.hasPermission("adminmode.admin")) {
-					if (inAdmin.contains(player.getName())) {
-						for (String command : adminmodedisable) {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+            if (inMod.contains(player.getName())) {
+                for (String command : modmodedisable) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+                }
+                inMod.remove(player.getName());
+                if (StaffLocations.containsKey(player.getName())) {
+                    player.teleport(StaffLocations.get(player.getName()));
+                    StaffLocations.remove(player.getName());
+                }
+                if (StaffInventories.containsKey(player.getName())) {
+                    player.getInventory().clear();
+                    player.getInventory().addItem(StaffInventories.get(player.getName()).getContents());
+                    player.getInventory().setExtraContents(StaffInventories.get(player.getName()).getExtraContents());
+                    player.getInventory().setArmorContents(StaffInventories.get(player.getName()).getArmorContents());
+                    StaffInventories.remove(player.getName());
+                }
+                if (StaffExp.containsKey(player.getName())) {
+                    player.setTotalExperience(StaffExp.get(player.getName()));
+                    StaffExp.remove(player.getName());
+                }
 
-						}
-						inAdmin.remove(player.getName());
-						if (StaffLocations.containsKey(player.getName())) {
-							player.teleport(StaffLocations.get(player.getName()));
-							StaffLocations.remove(player.getName());
-						}
-						return true;
-					} else {
-						for (String command : adminmodeenable) {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+                return true;
+            } else {
+                for (String command : modmodeenable) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
 
-						}
-						inAdmin.add(player.getName());
-						StaffLocations.put(player.getName(), player.getLocation());
-						getLogger().info(player.getName() + " entered admin mode");
-						return true;
-					}
+                }
+                inMod.add(player.getName());
+                StaffLocations.put(player.getName(), player.getLocation());
+                StaffInventories.put(player.getName(), player.getInventory());
+                StaffExp.put(player.getName(), player.getTotalExperience());
+                getLogger().info(player.getName() + " entered mod mode");
+                return true;
+            }
 
-				}
-			}
-		} else {
+        }
 
-			sender.sendMessage("You must be a player!");
-			return false;
-		}
+        if (cmd.getName().equalsIgnoreCase("adminmode")) {
+            if (!player.hasPermission("adminmode.admin")) {
+                sender.sendMessage("You don't have permission to do that");
+                return true;
+            }
+            if (inMod.contains(player.getName())) {
+                sender.sendMessage("You're already in mod mode!");
+                return true;
+            }
 
-		return false;
-	}
+            if (inAdmin.contains(player.getName())) {
+                for (String command : adminmodedisable) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+
+                }
+                inAdmin.remove(player.getName());
+                if (StaffLocations.containsKey(player.getName())) {
+                    player.teleport(StaffLocations.get(player.getName()));
+                    StaffLocations.remove(player.getName());
+                }
+                if (StaffInventories.containsKey(player.getName())) {
+                    player.getInventory().clear();
+                    player.getInventory().addItem(StaffInventories.get(player.getName()).getContents());
+                    player.getInventory().setExtraContents(StaffInventories.get(player.getName()).getExtraContents());
+                    player.getInventory().setArmorContents(StaffInventories.get(player.getName()).getArmorContents());
+                    StaffInventories.remove(player.getName());
+                }
+                if (StaffExp.containsKey(player.getName())) {
+                    player.setTotalExperience(StaffExp.get(player.getName()));
+                    StaffExp.remove(player.getName());
+                }
+                return true;
+            } else {
+                for (String command : adminmodeenable) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+
+                }
+                inAdmin.add(player.getName());
+                StaffLocations.put(player.getName(), player.getLocation());
+                StaffInventories.put(player.getName(), player.getInventory());
+                StaffExp.put(player.getName(), player.getTotalExperience());
+                getLogger().info(player.getName() + " entered admin mode");
+                return true;
+            }
+
+        }	
+        return false;
+    }
 }
