ad8093a7d0c7420acfbfb6c836fee2e3e5c8983b
cccm5 <danieldavidson@aol.com>
2017-06-25 19:11:03 -0400

    Allow creation of dynamic modes. Warning: the configuration now uses multiple files instead of a singular file.

--- src/main/java/io/github/thomdare/adminmode/Main.java
+++ src/main/java/io/github/thomdare/adminmode/Main.java
@@ -1,229 +1,120 @@
 package io.github.thomdare.adminmode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.command.Command;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-    private List<String> adminmodeenable = new ArrayList<String>();
-    private List<String> modmodeenable = new ArrayList<String>();
-    private List<String> adminmodedisable = new ArrayList<String>();
-    private List<String> modmodedisable = new ArrayList<String>();
-    private List<String> inMod = new ArrayList<String>();
-    private List<String> inAdmin = new ArrayList<String>();
-    private HashMap<String, Location> StaffLocations = new HashMap<String, Location>();
-    private HashMap<String, ItemStack[]> StaffInventories = new HashMap<String, ItemStack[]>();
-    private HashMap<String, Integer> StaffLevel = new HashMap<String, Integer>();
-    private HashMap<String, Float> StaffExp = new HashMap<String, Float>();
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public final class Main extends JavaPlugin {
+    private List<Mode> modes = new ArrayList<>();
 
     public void onEnable() {
-        // We need these
-        getConfig().addDefault("permissions.adminmode.onenable", adminmodeenable);
-        getConfig().addDefault("permissions.modmode.onenable", modmodeenable);
-        getConfig().addDefault("permissions.adminmode.ondisable", adminmodedisable);
-        getConfig().addDefault("permissions.modmode.ondisable", modmodedisable);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
-        // Lets get those already there
-        adminmodeenable = getConfig().getStringList("permissions.adminmode.onenable");
-        modmodeenable = getConfig().getStringList("permissions.modmode.onenable");
-        adminmodedisable = getConfig().getStringList("permissions.adminmode.ondisable");
-        modmodedisable = getConfig().getStringList("permissions.modmode.ondisable");
+        loadModes();
     }
 
     public void onDisable() {
-        for(String name : inMod){
-            Player player = Bukkit.getPlayer(name);
-            for (String command : modmodedisable) {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", name));
+        for(Mode mode : modes){
+            for(UUID id : mode.getPlayers()){
+                mode.disableMode(Bukkit.getPlayer(id));
             }
-            if (StaffLocations.containsKey(name)) {
-                player.teleport(StaffLocations.get(name));
-                StaffLocations.remove(name);
-            }
-            if (StaffInventories.containsKey(name)) {
-                player.getInventory().clear();
-                //player.getInventory().addItem(StaffInventories.get(player.getName()));
-                for (int i = 0; i <StaffInventories.get(name).length;i++) {
-                    if(StaffInventories.get(name)[i] == null)
-                        continue;
-                    player.getInventory().setItem(i,StaffInventories.get(name)[i]);
-                }
-                StaffInventories.remove(name);
-            }
-            if (StaffExp.containsKey(name)) {
-                player.setExp(StaffExp.get(name));
-                player.setLevel(StaffLevel.get(name));
-                StaffLevel.remove(name);
-                StaffExp.remove(name);
-            }
-            player.updateInventory();
         }
-        for(String name: inAdmin){
-            Player player = Bukkit.getPlayer(name);
-            for (String command : adminmodedisable) {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p",name));
-
-            }
-            if (StaffLocations.containsKey(name)) {
-                player.teleport(StaffLocations.get(name));
-                StaffLocations.remove(name);
-            }
-            if (StaffInventories.containsKey(name)) {
-                player.getInventory().clear();
-                //player.getInventory().addItem(StaffInventories.get(name));
-                for (int i = 0; i <StaffInventories.get(name).length;i++) {
-                    if(StaffInventories.get(name)[i] == null)
-                        continue;
-                    player.getInventory().setItem(i,StaffInventories.get(name)[i]);
-                }
-                StaffInventories.remove(name);
-            }
-            if (StaffExp.containsKey(name)) {
-                player.setExp(StaffExp.get(name));
-                player.setLevel(StaffLevel.get(name));
-                StaffLevel.remove(name);
-                StaffExp.remove(name);
-            }
-            player.updateInventory();   
-        }
-        inMod.clear();
-        inAdmin.clear();
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("You must be a player to execute that command!");
+
+    private void loadModes() {
+        if (getDataFolder().listFiles() == null || getDataFolder().listFiles().length == 0) {
+            File exampleFile = new File(getDataFolder(), "exampleMode.yml");
+            exampleFile.getParentFile().mkdirs();
+            saveResource("exampleMode.yml", false);
+        }
+        assert getDataFolder().listFiles() != null;
+
+        List<File> fileConfigs = Arrays.asList(getDataFolder().listFiles());
+        List<YamlConfiguration> fileYaml = new ArrayList<>(fileConfigs.size());
+        try {
+            for (int i = 0; i < fileYaml.size(); i++) {
+                fileYaml.get(i).load(fileConfigs.get(i));
+                modes.add(new Mode(fileYaml.get(i), fileConfigs.get(i)));
+            }
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        for(Mode mode : modes) {
+            try {
+                final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+
+                bukkitCommandMap.setAccessible(true);
+                CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+
+                commandMap.register(mode.getName(), new ModeCommand(mode.getName()));
+            } catch (IllegalAccessException | SecurityException | NoSuchFieldException | IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param player the player to check
+     * @return the mode a player is in, otherwise null
+     */
+    private Mode getPlayerMode(Player player) {
+        for (Mode mode : modes) {
+            if (mode.getPlayers().contains(player.getUniqueId()))
+                return mode;
+        }
+        return null;
+    }
+
+    private Mode getMode(String name) {
+        for (Mode mode : modes) {
+            if (mode.getName().equals(name))
+                return mode;
+        }
+        return null;
+    }
+
+    private class ModeCommand extends BukkitCommand {
+
+        private ModeCommand(String name) {
+            super(name);
+            this.description = "Toggles " + name + " mode.";
+            this.usageMessage = "/" + name;
+            this.setPermission("StaffMode." + name);
+            this.setAliases(new ArrayList<String>());
+        }
+
+        @Override
+        public boolean execute(CommandSender sender, String s, String[] strings) {
+            if (!sender.hasPermission(this.getPermission())) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission.");
+                return true;
+            }
+            Player player = (Player) sender;
+            Mode playerMode = getPlayerMode(player);
+            if (playerMode != getMode(getName()) && playerMode != null) {
+                sender.sendMessage("You're already in " + getPlayerMode(player) + " mode!");
+                return true;
+            }
+
+            if (playerMode != null) {
+                return true;
+            }
             return true;
         }
 
-        Player player = (Player) sender;
-        if (cmd.getName().equalsIgnoreCase("modmode")) {
-            if (!player.hasPermission("adminmode.mod")) {
-                sender.sendMessage("You don't have permission to do that");
-                return true;
-            }
-            if (inAdmin.contains(player.getName())) {
-                sender.sendMessage("You're already in admin mode!");
-                return true;
-            }
-
-            if (inMod.contains(player.getName())) {
-                for (String command : modmodedisable) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
-                }
-                inMod.remove(player.getName());
-                if (StaffLocations.containsKey(player.getName())) {
-                    player.teleport(StaffLocations.get(player.getName()));
-                    StaffLocations.remove(player.getName());
-                }
-                if (StaffInventories.containsKey(player.getName())) {
-                    player.getInventory().clear();
-                    //player.getInventory().addItem(StaffInventories.get(player.getName()));
-                    for (int i = 0; i <StaffInventories.get(player.getName()).length;i++) {
-                        if(StaffInventories.get(player.getName())[i] == null)
-                            continue;
-                        player.getInventory().setItem(i,StaffInventories.get(player.getName())[i]);
-                    }
-                    StaffInventories.remove(player.getName());
-                }
-                if (StaffExp.containsKey(player.getName())) {
-                    player.setExp(StaffExp.get(player.getName()));
-                    player.setLevel(StaffLevel.get(player.getName()));
-                    StaffExp.remove(player.getName());
-                    StaffLevel.remove(player.getName());
-                }
-                player.updateInventory();
-                return true;
-            } else {
-                for (String command : modmodeenable) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
-
-                }
-                inMod.add(player.getName());
-                StaffLocations.put(player.getName(), player.getLocation());
-                StaffInventories.put(player.getName(), player.getInventory().getContents());
-                StaffExp.put(player.getName(), player.getExp());
-                StaffLevel.put(player.getName(), player.getLevel());
-                player.setExp(0);
-                player.setLevel(0);
-                player.getInventory().clear();
-                player.updateInventory();
-                getLogger().info(player.getName() + " entered mod mode");
-                sender.sendMessage("Entering mod mode");
-                return true;
-            }
-
-        }
-
-        if (cmd.getName().equalsIgnoreCase("adminmode")) {
-            if (!player.hasPermission("adminmode.admin")) {
-                sender.sendMessage("You don't have permission to do that");
-                return true;
-            }
-            if (inMod.contains(player.getName())) {
-                sender.sendMessage("You're already in mod mode!");
-                return true;
-            }
-
-            if (inAdmin.contains(player.getName())) {
-                for (String command : adminmodedisable) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
-
-                }
-                inAdmin.remove(player.getName());
-                if (StaffLocations.containsKey(player.getName())) {
-                    player.teleport(StaffLocations.get(player.getName()));
-                    StaffLocations.remove(player.getName());
-                }
-                if (StaffInventories.containsKey(player.getName())) {
-                    player.getInventory().clear();
-                    //player.getInventory().addItem(StaffInventories.get(player.getName()));
-                    for (int i = 0; i <StaffInventories.get(player.getName()).length;i++) {
-                        if(StaffInventories.get(player.getName())[i] == null)
-                            continue;
-                        player.getInventory().setItem(i,StaffInventories.get(player.getName())[i]);
-                    }
-                    StaffInventories.remove(player.getName());
-                }
-                if (StaffExp.containsKey(player.getName())) {
-                    player.setExp(StaffExp.get(player.getName()));
-                    player.setLevel(StaffLevel.get(player.getName()));
-                    StaffExp.remove(player.getName());
-                    StaffLevel.remove(player.getName());
-                }
-                player.updateInventory();
-                return true;
-            } else {
-                for (String command : adminmodeenable) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
-
-                }
-                inAdmin.add(player.getName());
-                StaffLocations.put(player.getName(), player.getLocation());
-                StaffInventories.put(player.getName(), player.getInventory().getContents());
-                StaffExp.put(player.getName(), player.getExp());
-                StaffLevel.put(player.getName(), player.getLevel());
-                player.setExp(0);
-                player.setLevel(0);
-                player.getInventory().clear();
-                player.updateInventory();
-                getLogger().info(player.getName() + " entered admin mode");
-                sender.sendMessage("Entering admin mode");
-                return true;
-            }
-
-        }	
-        return false;
     }
+
 }
---
+++ src/main/java/io/github/thomdare/adminmode/Mode.java
@@ -0,0 +1,112 @@
+package io.github.thomdare.adminmode;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+public class Mode {
+    private List<String> enableCommands;
+    private List<String> disableCommands;
+    private Map<UUID, StaffData> inMode = new HashMap<>();
+    private String name;
+
+    public Mode(FileConfiguration config, File file) throws IOException {
+        config.addDefault("enableCommands", enableCommands);
+        config.addDefault("disableCommands", disableCommands);
+        config.options().copyDefaults(true);
+        config.save(file);
+        enableCommands = config.getStringList("enableCommands");
+        disableCommands = config.getStringList("disableCommands");
+        name = config.getName();
+        Bukkit.getLogger().info("Loading mode: " + name);
+
+
+    }
+
+    public Set<UUID> getPlayers() {
+        return inMode.keySet();
+    }
+
+    public void enableMode(Player player) {
+        for (String command : enableCommands) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+
+        }
+        inMode.put(player.getUniqueId(), new StaffData(player.getLocation(), player.getLevel(), player.getExp(), player.getInventory().getContents()));
+        player.setExp(0);
+        player.setLevel(0);
+        player.getInventory().clear();
+        player.updateInventory();
+        Bukkit.getLogger().info(player.getName() + " entered mod mode");
+        player.sendMessage("Entering mod mode");
+    }
+
+    public void disableMode(Player player) {
+        for (String command : disableCommands) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("@p", player.getName()));
+        }
+        inMode.remove(player.getUniqueId());
+        StaffData playerData = inMode.get(player.getUniqueId());
+        player.teleport(playerData.getLoc());
+
+        player.getInventory().clear();
+        //player.getInventory().addItem(StaffInventories.get(player.getName()));
+        for (int i = 0; i < playerData.getInv().length; i++) {
+            if (playerData.getInv()[i] == null)
+                continue;
+            player.getInventory().setItem(i, playerData.getInv()[i]);
+        }
+
+        player.setExp(playerData.getXp());
+        player.setLevel(playerData.getXpLevel());
+        player.updateInventory();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private class StaffData {
+        private final Location loc;
+        private final int xpLevel;
+        private final float xp;
+        private final ItemStack[] inv;
+
+        public StaffData(Location loc, int xpLevel, float xp, ItemStack... inv) {
+
+            this.loc = loc;
+            this.xpLevel = xpLevel;
+            this.xp = xp;
+            this.inv = inv;
+        }
+
+        public Location getLoc() {
+            return loc;
+        }
+
+        public int getXpLevel() {
+            return xpLevel;
+        }
+
+        public float getXp() {
+            return xp;
+        }
+
+        public ItemStack[] getInv() {
+            return inv;
+        }
+    }
+
+
+}
